9940edac98802387b68f226ac68033d6a16d34ba
Anujna P Rao <46913368+anujnaRao@users.noreply.github.com>
2020-07-02 15:47:46 +0530

    Example of regular expression

---
+++ RegExp2.js
@@ -0,0 +1,12 @@
+function regexVar() {
+    const re = /^(Mr|Mrs|Ms|Dr|Er)(\.)([a-zA-Z])*$/;
+    return re;
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(!!s.match(re));
+}
